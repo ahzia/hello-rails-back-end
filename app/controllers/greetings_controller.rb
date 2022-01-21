ac6f75ea96496e515932b72b68cdbfc6ff326d68
ahzia <ah.ziayosfi@gmail.com>
2022-01-22 00:05:10 +0430

    fix linters issue

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,5 +1,5 @@
 class GreetingsController < ApplicationController
   def random
-      render json: { greeting: Greeting.find(rand(1..5)) }.to_json
+    render json: { greeting: Greeting.find(rand(1..5)) }.to_json
   end
 end
